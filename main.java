dfacc9ca59252db4ea6cc7f5e15657890d1f7cb8
Yan Gorshtenin <support@netlodge.net>
2014-05-17 15:38:54 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,42 @@
+package yan;
+
+public class main {
+
+
+	public static void main(String[] args) {
+		
+
+		for (int verse =1; verse <= 3; verse++) {
+			System.out.print("Al's all wet.");
+			System.out.println("Oh, Why is Al all wet? Oh,");
+			System.out.print("Als all wet 'cause");
+			System.out.println("He's standing in the rain.");
+			System.out.println("Why is Al standing out in the rain?");
+			
+			switch (verse) {
+			case 1:
+				System.out.println("That's because he has no brain.");
+					break;
+			case 2:
+				System.out.println("That's because he is a pain.");
+					break;
+			case 3:
+				System.out.println("'Cause this is the last refrain.");
+					break;
+			}
+			
+			switch (verse) {
+			case 3:
+				System.out.println("Last refrain, last");
+			case 2:
+				System.out.println("He's a pain, he's a pain,");
+			case 1:
+				System.out.println("Has no brain, has no brain,");
+			}
+			
+		}
+		System.out.println("In the rain, in the rain.");
+	}
+
+	}
+
